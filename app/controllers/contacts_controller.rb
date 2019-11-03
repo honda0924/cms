25ff9b77de41ceeec8afb76254accde588bc7559
honda0924 <honda0924@hotmail.co.jp>
2019-11-03 13:37:14 +0900

    create_action_update

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,6 +9,16 @@ class ContactsController < ApplicationController
     Contact.create(contact_params)
     redirect_to client_path(params[:client_id])
   end
+  def show
+    @contact=Contact.find(params[:id])
+    @client=@contact.client
+  end
+  def update
+    contact=Contact.find(params[:id])
+    client=contact.client
+    contact.update(contact_params)
+    redirect_to client_path(params[:client_id])
+  end
 
   private
   def contact_params
